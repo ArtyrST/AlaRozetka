a2a7d05feb0f6fcd427e18a2256485e76e4c194b
ArtyrST <stadnikartyr@gmail.com>
2026-03-17 18:33:16 +0200

    Create a 'Entity' folder, and add ProductEntity class

---
+++ BackEnd/AlaBackEnd/AlaBackEnd/Controllers/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AlaBackEnd.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlaBackEnd.Controllers
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ProductEntity> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            string DbConnectionString = "Server=localhost\\MSSQLSERVER03;Database=master;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(DbConnectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+
+            builder.Entity<ProductEntity>()
+                .HasKey(p => p.Id);
+
+        }
+    }
+}
---
+++ BackEnd/AlaBackEnd/AlaBackEnd/Entity/ProductEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace AlaBackEnd.Entity
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
