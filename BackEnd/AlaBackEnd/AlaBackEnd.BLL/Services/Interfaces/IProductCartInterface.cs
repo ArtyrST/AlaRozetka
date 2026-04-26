4b58f4d5b0392365129dfdc5bcf5648e3a39fa10
ArtyrST <stadnikartyr@gmail.com>
2026-04-27 02:12:34 +0300

    ProductCart controller method

---
+++ BackEnd/AlaBackEnd/AlaBackEnd.BLL/Services/Interfaces/IProductCartInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlaBackEnd.BLL.Services.Interfaces
+{
+    public interface IProductCartInterface
+    {
+        Task<ServiceResponse> GetUserCartAsync();
+    }
+}
---
+++ BackEnd/AlaBackEnd/AlaBackEnd.DAL/Repositories/ProductCartRepository.cs
@@ -0,0 +1,29 @@
+﻿using AlaBackEnd.DAL.Entity.ProductCart;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlaBackEnd.DAL.Repositories
+{
+    public class ProductCartRepository : GenericRepository<CartEntity>
+    {
+        private readonly AppDbContext _context;
+        public ProductCartRepository(AppDbContext context)
+            : base(context)
+        {
+            _context = context;
+        }
+        public IQueryable<CartEntity> carts => GetAll()
+            .Include(c => c.OrderItems)
+            .Include(c => c.User)
+            .AsSingleQuery();
+            //.AsNoTracking();
+        public async Task<CartEntity?> GetByUserIdAsync(int id)
+        {
+            return await carts
+                .FirstOrDefaultAsync(c => c.UserId == id);
+                
+        }
+    }
+}
